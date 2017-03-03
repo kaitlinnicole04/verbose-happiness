e1746efdf6bb6eeeb0990d9b0b88d5c141ab2c95
Kaitlin <kaitlinanderson04@gmail.com>
2017-03-03 00:34:19 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   resources :line_items
   resources :carts
   get 'store/index'
-
+  
+  resources :users
   resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'products#index'
-  root 'store#index'
+  root  'store#index'
 end
