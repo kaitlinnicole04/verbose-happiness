8505cba97103a69e2c353bc1c09eb1f98dc47991
Kaitlin <kaitlinanderson04@gmail.com>
2017-03-16 01:51:00 +0000

    Update 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+  controller :sessions do
+  get 'login' => :new
+  post 'login' => :create
+  delete 'logout' => :destroy
+  end 
+
   resources :orders
   resources :line_items
   resources :carts
-  get 'store/index'
-  
   resources :users
-  resources :products
   #root 'products#index'
-  root  'store#index'
+  root  'store#index', as: 'store_index'
+   resources :products
 end
